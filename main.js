1d6938ecf0426152de7cef92b96687c900319253
Damian Cur <damian.cur91@gmail.com>
2021-03-18 19:12:34 +0100

    restructured code

--- main.js
+++ main.js
@@ -7,49 +7,59 @@ const h1 = document.querySelector("h1");
 
 const options = ["walcz", "przemyśl to jeszcze raz"];
 
+
 const addAdvice = (e) => {
     e.preventDefault();
     let newOption = input.value;
-    console.log(newOption);
     if (newOption.length === 0) {
-        alert("Uzupełnij puste pole.");
+        alert("Uzupełnij pole");
         return
     }
 
-    for(newOption of options) {
-        if (newOption === input.value) {
+    for (option of options) {
+        if (option === newOption) {
             alert("Już to dodałeś");
+            input.value = "";
             return
         }
     }
-    
-
     options.push(newOption);
     input.value = "";
 }
 
 const cleanOptions = (e) => {
     e.preventDefault();
-    options.length = "";
-    alert("Poprawnie zresetowano możliwości.")
+    options.length = 0;
+    alert("Poprawnie zresetowano wprowadzone dane.");
 }
 
 const showContent = (e) => {
     e.preventDefault();
-    alert(`Oto Twoje dodane możliwości: ${options}.`)
-}
 
+    for (let i = 0; i < options.length; i++) {
+        options[i] += " "
+    }
+
+    alert(`Oto Twoje dostępne możliwości: ${options}`);
+}
 
 const randomAdvice = (e) => {
     e.preventDefault();
-    if(options.length ===0) {
-        alert("Dodaj możliwości.");
-    }
-
     const index = Math.floor(Math.random() * options.length);
     h1.textContent = options[index];
+   
     
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
