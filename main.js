5be4da7455783fb042f5cd34fb21a65b3e160d41
Damian Cur <damian.cur91@gmail.com>
2021-03-18 17:10:38 +0100

    added "adding options" , "reset" feature

--- main.js
+++ main.js
@@ -5,3 +5,24 @@ const showAdvice = document.querySelector(".showAdvice");
 const showOptions = document.querySelector(".showOptions");
 
 const options = ["walcz", "przemyśl to jeszcze raz"];
+
+const addAdvice = (e) => {
+    e.preventDefault();
+   const userInput = input.value;
+   options.push(userInput);
+   input.value = "";
+}
+
+const cleanOptions = (e) => {
+    e.preventDefault();
+    options.length = "";
+    alert("Poprawnie zresetowano możliwości.")
+}
+
+
+
+
+
+
+clean.addEventListener("click" , cleanOptions);
+add.addEventListener("click", addAdvice);
